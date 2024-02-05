f9eb8ad6de40716ea6be268426c383c4ede654de
Nagacash <chosenfewrecords@hotmail.de>
2024-02-05 18:05:54 +0100

    second

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="http://naga-store.vercel.app/stripe/success"
+      cancelUrl="http://naga-store.vercel.app/stripe/error"
       currency="EUR"
       billingAddressCollection={false}
       shouldPersist={true}
